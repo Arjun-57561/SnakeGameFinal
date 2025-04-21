e70508cfecac60abaa2b2997b0639f31f19b755c
Arjun Reddy <160053005+Arjun-57561@users.noreply.github.com>
2025-04-21 06:46:19 +0530

    Adding custom Exception 3

---
+++ ScoreRecordingException.java
@@ -0,0 +1,9 @@
+public class ScoreRecordingException extends Exception {
+    public ScoreRecordingException(String message) {
+        super(message);
+    }
+    
+    public ScoreRecordingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
